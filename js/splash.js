538041d970751b525f2438f6d716483e51495b77
tiagop85 <tiagoparente85@gmail.com>
2017-06-09 20:49:45 -0400

    commit teste

--- js/splash.js
+++ js/splash.js
@@ -8,10 +8,10 @@ SplashState.prototype.preload = function() {
 
 SplashState.prototype.create = function() {
     this.game.add.sprite(0,0, 'bgSplash')
-//    game.sound.mute = true;
+//    game.sound.mute = true; isso e um teste
     
 //    var sprite = this.game.add.sprite(0, 0, 'bgSplash');
-//    sprite.alpha = 0;
+//    sprite.alpha = 0; versao final ok
 //    this.game.add.tween(sprite).to( { alpha: 1 }, 2000, Phaser.Easing.Linear.None, false, 0, 1000, true);
     
     this.game.time.events.add(Phaser.Timer.SECOND * 0.2, gotoMenu, this);
